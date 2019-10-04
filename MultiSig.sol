9bd0ca1c52d7340b2e4b5dec1a15e76e5214cae7
Medha Pandey <medha8pandey@gmail.com>
2019-10-04 09:38:13 +0530

    Update

--- MultiSig.sol
+++ MultiSig.sol
@@ -3,6 +3,25 @@ pragma solidity ^0.5.0;
 import "browser/Verification.sol";
 
 contract MultiSig{
+
+// web3 generate digest 
+// Signature Verification - ecrecover 
+// Multi Sig Wallet sig1 -> verify  sig2 -> verify -> state var change 
+// State Channel open close challenge claim 
+
+// open 
+// p1 p2 -> eth 
+//can not submit eth now
+
+
+// close 
+// state var value change 
+//if all parameter such as nonce p1,p2 -> sig1 sig2  correct
+
+//Web3 -> "nonce-p1-p2" eg. ("337") -> sign from p1 private key -> sig1
+                          //("337") -> sign from p2 private key -> sig2
+                          
+//close(nonce,p1,p2,sig1,sig2) -> covert everything to string -> concat -> "337" -> hash("337") --> ecrecover(hash,sig1) == p1 && ecrecover(hash,sig2) == p2
     
     // take hash message and verify the signature from address is same as address given 
     // take the message and verify the second signature is also same 
